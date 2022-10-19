0196f831288cd8c2a4d41eb4847ed80dfda0966e
Ruiyotabby <ruiyotabby@gmail.com>
2022-10-19 23:08:20 +0300

    Add append method which adds value to the end

--- linked_lists.rb
+++ linked_lists.rb
@@ -10,6 +10,7 @@ end
 
 # This object contains the list to the linked list
 class LinkedList
+  attr_accessor :head, :tail
 
   def initialize
     @head = nil
@@ -20,10 +21,25 @@ class LinkedList
     if @head.nil?
       @head = value
       @tail = value
+    else
+      value.next_node = @head
+      @head = value
     end
-    value.next_node = @head
-    @head = value
+  end
+
+  def append(value)
+    if @tail.nil?
+      @head = value
+      @tail = value
+    else
+      @tail.next_node = value
+      @tail = value
+    end
   end
 end
 
 m = Node.new
+m.data = 4
+L = LinkedList.new
+L.prepend(m)
+print L.tail.data
