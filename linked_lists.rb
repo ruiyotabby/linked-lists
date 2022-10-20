5b0ca75ba76caba7f35a369895dc537adffbfc70
Ruiyotabby <ruiyotabby@gmail.com>
2022-10-20 09:30:53 +0300

    Add an at method wich returns the node at a given index

--- linked_lists.rb
+++ linked_lists.rb
@@ -10,7 +10,7 @@ end
 
 # This object contains the list to the linked list
 class LinkedList
-  attr_accessor :head, :tail
+  attr_reader :head, :tail
 
   def initialize
     @head = nil
@@ -18,31 +18,52 @@ class LinkedList
   end
 
   def prepend(value)
-    if @head.nil?
-      @tail = value
-    else
-      value.next_node = @head
+    unless value.class == Node
+      node = Node.new
+      node.data = value
     end
-    @head = value
+    node = value if value.class == Node
+    if @head.nil?
+      @tail = node
+    else
+      node.next_node = @head
+    end
+    @head = node
   end
 
   def append(value)
-    if @tail.nil?
-      @head = value
-    else
-      @tail.next_node = value
+    unless value.class == Node
+      node = Node.new
+      node.data = value
     end
-    @tail = value
+    node = value if value.class == Node
+    if @tail.nil?
+      @head = node
+    else
+      @tail.next_node = node
+    end
+    @tail = node
   end
 
   def size
-    num = @head
-    i = 0
-    while !num.nil?
-      i += 1
-      num = num.next_node
+    node = @head
+    count = 0
+    until node.nil?
+      node = node.next_node
+      count += 1
     end
-    i
+    count
+  end
+
+  def at(index)
+    return nil if index > size
+    count = 0
+    node = @head
+    loop do
+      count += 1
+      node = node.next_node
+      return node if count == index
+    end
   end
 end
 
@@ -55,9 +76,12 @@ k = Node.new
 k.data = 234
 n.next_node = k
 L = LinkedList.new
-L.prepend(m)
-L.append(n)
-# L.append(k)
-# p L.head
+# L.prepend(m)
+# L.append(n)
+L.append(566)
+L.prepend(10)
+L.append(k)
+p L.head
 p L.tail
-# p L.size
+p L.size
+p L.at(1)
